ef4f8e40c6f636ae47d22feac00f478263e41849
Muhammadqodir Abduraxmanov <60865598+Muhammad-uzbek@users.noreply.github.com>
2022-08-19 19:03:47 +0500

    Update about.js

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -134,39 +134,43 @@ const About = () => {
       <div className="inner">
         <StyledText>
           <div>
-            <p>
-              Hello! My name is Brittany and I enjoy creating things that live on the internet. My
-              interest in web development started back in 2012 when I decided to try editing custom
-              Tumblr themes — turns out hacking together a custom reblog button taught me a lot
-              about HTML &amp; CSS!
-            </p>
+               <p>
+                   Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                   Sed ut  <a href="https://us.mullenlowe.com/">perspiciatis</a>,{' '} unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.
+               </p>
+//             <p>
+//               Hello! My name is Brittany and I enjoy creating things that live on the internet. My
+//               interest in web development started back in 2012 when I decided to try editing custom
+//               Tumblr themes — turns out hacking together a custom reblog button taught me a lot
+//               about HTML &amp; CSS!
+//             </p>
 
-            <p>
-              Fast-forward to today, and I’ve had the privilege of working at{' '}
-              <a href="https://us.mullenlowe.com/">an advertising agency</a>,{' '}
-              <a href="https://starry.com/">a start-up</a>,{' '}
-              <a href="https://www.apple.com/">a huge corporation</a>, and{' '}
-              <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
-              main focus these days is building accessible, inclusive products and digital
-              experiences at <a href="https://upstatement.com/">Upstatement</a> for a variety of
-              clients.
-            </p>
+//             <p>
+//               Fast-forward to today, and I’ve had the privilege of working at{' '}
+//               <a href="https://us.mullenlowe.com/">an advertising agency</a>,{' '}
+//               <a href="https://starry.com/">a start-up</a>,{' '}
+//               <a href="https://www.apple.com/">a huge corporation</a>, and{' '}
+//               <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
+//               main focus these days is building accessible, inclusive products and digital
+//               experiences at <a href="https://upstatement.com/">Upstatement</a> for a variety of
+//               clients.
+//             </p>
 
-            <p>
-              I also recently{' '}
-              <a href="https://www.newline.co/courses/build-a-spotify-connected-app">
-                launched a course
-              </a>{' '}
-              that covers everything you need to build a web app with the Spotify API using Node
-              &amp; React.
-            </p>
+//             <p>
+//               I also recently{' '}
+//               <a href="https://www.newline.co/courses/build-a-spotify-connected-app">
+//                 launched a course
+//               </a>{' '}
+//               that covers everything you need to build a web app with the Spotify API using Node
+//               &amp; React.
+//             </p>
 
-            <p>Here are a few technologies I’ve been working with recently:</p>
+//             <p>Here are a few technologies I’ve been working with recently:</p>
           </div>
 
-          <ul className="skills-list">
-            {skills && skills.map((skill, i) => <li key={i}>{skill}</li>)}
-          </ul>
+//           <ul className="skills-list">
+//             {skills && skills.map((skill, i) => <li key={i}>{skill}</li>)}
+//           </ul>
         </StyledText>
 
         <StyledPic>
