709749b0624214f1f677324d6bde3b0fb4859978
Muhammadqodir Abduraxmanov <60865598+Muhammad-uzbek@users.noreply.github.com>
2022-08-21 14:58:14 +0500

    Update variables.js

--- src/styles/variables.js
+++ src/styles/variables.js
@@ -2,7 +2,7 @@ import { css } from 'styled-components';
 
 const variables = css`
   :root {
-    --dark-navy: #020c1b;
+    --dark-navy: #pf0582f;
     --navy: #0a192f;
     --light-navy: #112240;
     --lightest-navy: #233554;
